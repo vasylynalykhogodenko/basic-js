ceda3b01781932bf8236fab3e12a318518bbba67
Vasylyna Lykhogodenko <devvasylyna@gmail.com>
2025-01-06 21:51:03 +0200

    feat: create variable counter

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let counter = 0;
+
+  for (let rowOfCats of matrix) {
+    for (let cellOfCat of rowOfCats) {
+      if (cellOfCat === '^^') {
+        counter++;
+      }
+    }
+  }
+
+  return counter;
 }
 
 module.exports = {
